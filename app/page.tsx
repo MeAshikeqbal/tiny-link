c393144c680241ae0adc3c51cf56429de2bb328e
Ashik Eqbal <hello@itsashik.dev>
2025-11-30 05:45:54 +0000

    chore: add dynamic export to force dynamic rendering for DashboardPage

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import { columns } from "@/components/dashboard/columns"
 import { DashboardTableHeader } from "@/components/dashboard/dashboard-table-header"
 
 
+export const dynamic = 'force-dynamic'
+
 export default async function DashboardPage() {
   const links = await prisma.link.findMany({
     where: { deleted: false },
